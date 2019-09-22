5b32cf34c887e0133785466bfb3eeee6a3648648
gkdbs6862 <39699061+gkdbs6862@users.noreply.github.com>
2019-09-22 16:51:05 +0900

    Add files via upload

---
+++ pic.sh
@@ -0,0 +1,44 @@
+cd /data
+mkdir ThunderImages
+cd ThunderImages
+mkdir Thunder
+mkdir NotThunder
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/test_Thunder.jpg
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/test_NotThunder.jpg
+
+cd Thunder
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder1.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder2.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder3.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder4.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder5.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder6.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder7.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/Thunder/Thunder8.jpg
+
+cd ..
+cd NotThunder
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder1.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder2.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder3.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder4.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder5.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder6.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder7.jpg
+
+wget https://raw.githubusercontent.com/gkdbs6862/DigitsImages/master/Images/NotThunder/NotThunder8.jpg
